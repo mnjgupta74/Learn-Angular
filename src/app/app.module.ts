cbdb7f895ddd4a7208640f8541fa1aef038be58a
ManojGupta <mnjgupta74@gmail.com>
2020-10-08 15:20:03 +0530

    Commit and add navigation works in Working Directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyContainerComponentClass } from './MyContainer/MyContainer.component';
+import { MyContainer1Component } from './my-container1/my-container1.component';
+import { TopnavComponent } from './MyContainer/topnav/topnav.component';
+import { HeaderComponent } from './MyContainer/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    ,MyContainerComponentClass, MyContainer1Component, TopnavComponent, HeaderComponent
   ],
   imports: [
     BrowserModule,
